0e710d238e2c20e6e4bfac3695cb62beaa50b56a
bladeefan2011 <eetosjar@gmail.com>
2025-11-28 20:08:11 +0200

    öl

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,39 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+
+class All:
+    def test(self, player):
+        return True
+    
+class Not:
+    def __init__(self, condition):
+        self._condition = condition
+
+    def test(self, player):
+        return not self._condition.test(player)
+    
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+    
+
+class Or:
+    def __init__(self, *conditions):
+        self._conditions = conditions
+
+    def test(self, player):
+        for i in self._conditions:
+            if i.test(player):
+                return True
+            
+        return False
